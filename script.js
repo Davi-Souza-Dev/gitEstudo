e60762916e07026dc9fee724babc605428aea828
Davi-Souza-Dev <daviekalebe2200@gmail.com>
2024-12-31 08:46:54 -0300

    Mudança na main

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const cubo = document.getElementById("cubo");
 btnHide.addEventListener("click",()=>{
     cubo.style.display = cubo.style.display == "none" ? "flex" : "none";
 })
+
+btnHide.addEventListener("mouseout",()=>{
+    console.log("VIADO");
+})
+

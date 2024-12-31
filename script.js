cc251feede273409b47dffb7b2a9dabc0dbb981d
Davi-Souza-Dev <daviekalebe2200@gmail.com>
2024-12-31 08:55:36 -0300

    Refazendo as mudanças

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const cubo = document.getElementById("cubo");
 btnHide.addEventListener("click",()=>{
     cubo.style.display = cubo.style.display == "none" ? "flex" : "none";
 })
+
+btnHide.addEventListener("mouseout",()=>{
+    console.log("VIADO");
+})
